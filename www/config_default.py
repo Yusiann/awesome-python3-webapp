fbbd5c031876ceb90eef4318df0683938522e3fe
Yusiann <412886100@qq.com>
2022-01-10 19:59:32 +0800

    第六天

---
+++ www/config_default.py
@@ -0,0 +1,20 @@
+
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Default configurations.默认设置
+'''
+configs = {
+    'debug': True,
+    'db': {
+        'host': '127.0.0.1',
+        'port': 3306,
+        'user': 'root',
+        'password': 'ann2008123',
+        'db': 'myworld'
+    },
+    'session': {
+        'secret': 'Myworld'
+    }
+}
---
+++ www/config_override.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+'''
+Override configurations.覆盖配置
+'''
+
+configs = {
+    'db': {
+        'host': '127.0.0.1'
+    }
+}
